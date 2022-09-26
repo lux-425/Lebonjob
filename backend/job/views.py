e9333bb9406623edac600fed2146f1f37c06e03a
Luke_425 <garcia.luc66@gmail.com>
2022-09-26 17:20:47 +0200

    Endpoints for creating, updating and deleting a job

--- backend/job/views.py
+++ backend/job/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import JobSerializer
 from .models import Job
@@ -27,3 +28,44 @@ def getJob(request, pk):
 
     serializer = JobSerializer(job, many=False)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def newJob(request):
+    data = request.data
+
+    job = Job.objects.create(**data)
+
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def updateJob(request, pk):
+    job = get_object_or_404(Job, id=pk)
+
+    job.title = request.data['title']
+    job.description = request.data['description']
+    job.email = request.data['email']
+    job.address = request.data['address']
+    job.jobType = request.data['jobType']
+    job.education = request.data['education']
+    job.industry = request.data['industry']
+    job.experience = request.data['experience']
+    job.salary = request.data['salary']
+    job.positions = request.data['positions']
+    job.company = request.data['company']
+
+    job.save()
+
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteJob(request, pk):
+    job = get_object_or_404(Job, id=pk)
+
+    job.delete()
+
+    return Response({'message': 'Job deleted successfully.'}, status=status.HTTP_200_OK)
