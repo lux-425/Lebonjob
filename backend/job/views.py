2cf037d7bb5b5217a59577089a6362c6473d3c97
Luke_425 <garcia.luc66@gmail.com>
2022-09-26 20:38:37 +0200

    Filters + pagination added to get all jobs' request

--- backend/job/views.py
+++ backend/job/views.py
@@ -1,9 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
 
-from .serializers import JobSerializer
 from .models import Job
+from .serializers import JobSerializer
+from .filters import JobsFilter
 
 from django.shortcuts import get_object_or_404
 from django.db.models import Avg, Min, Max, Count
@@ -14,10 +16,26 @@ from django.db.models import Avg, Min, Max, Count
 @api_view(['GET'])
 def getAllJobs(request):
 
-    jobs = Job.objects.all()
+    # jobs = Job.objects.all()
+    filterset = JobsFilter(
+        request.GET, queryset=Job.objects.all().order_by('id'))
 
-    serializer = JobSerializer(jobs, many=True)
-    return Response(serializer.data)
+    count = filterset.qs.count()
+
+    # Enable pagination
+    resPerPage = 3
+    paginator = PageNumberPagination()
+    paginator.page_size = resPerPage
+
+    # Apply pagination
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+
+    serializer = JobSerializer(queryset, many=True)
+    return Response({
+        'count': count,
+        'resPerPage': resPerPage,
+        'jobs': serializer.data
+    })
 
 
 @api_view(['GET'])
