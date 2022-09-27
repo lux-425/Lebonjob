d226f41fa385ae0251b198ffd0ebb9f0b2db3db6
Luke_425 <garcia.luc66@gmail.com>
2022-09-27 22:25:18 +0200

    Register, Login and Current User endpoints

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+
+from .serializers import SignUpSerializer, UserSerializer
+
+
 # Create your views here.
+
+@api_view(['POST'])
+def register(request):
+    data = request.data
+
+    user = SignUpSerializer(data=data)
+
+    if user.is_valid():
+        if not User.objects.filter(username=data['email']).exists():
+            user = User.objects.create(
+                first_name=data['first_name'],
+                last_name=data['last_name'],
+                email=data['email'],
+                username=data['email'],
+                password=make_password(data['password']),
+            )
+            return Response({
+                'message': 'New user registered successfully.'},
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response({
+                'error': 'User already registered with this email.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    else:
+        return Response(user.errors)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def currentUser(request):
+    user = UserSerializer(request.user)
+
+    return Response(user.data)
--- backend/job/views.py
+++ backend/job/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Job
 from .serializers import JobSerializer
