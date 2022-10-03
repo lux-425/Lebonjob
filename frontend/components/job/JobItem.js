69a01ea02caa344199afcdd3face798175ab87cb
Luke_425 <garcia.luc66@gmail.com>
2022-10-03 17:29:44 +0200

    Layout's main components and home page + global styling

---
+++ frontend/components/job/JobItem.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Link from 'next/link';
+
+const JobItem = () => {
+  return (
+    <Link href='/jobdetail'>
+      <a className='job-listing'>
+        <div className='job-listing-details'>
+          <div className='job-listing-description'>
+            <h4 className='job-listing-company'>Ciel</h4>
+            <h3 className='job-listing-title'>Full Stack Developer</h3>
+            <p className='job-listing-text'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+
+          <span className='bookmark-icon'></span>
+        </div>
+
+        <div className='job-listing-footer'>
+          <ul>
+            <li>
+              <i aria-hidden className='fas fa-industry'></i> IT
+            </li>
+
+            <li>
+              <i aria-hidden className='fas fa-briefcase'></i> Permanent
+            </li>
+            <li>
+              <i aria-hidden className='fas fa-money-check-alt'></i>$55555
+            </li>
+            <li>
+              <i aria-hidden className='far fa-clock'></i> Posted 2 weeks ago
+            </li>
+          </ul>
+        </div>
+      </a>
+    </Link>
+  );
+};
+
+export default JobItem;
---
+++ frontend/components/layout/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+const Header = () => {
+  return (
+    <div className='navWrapper'>
+      <div className='navContainer'>
+        <Link href='/'>
+          <div className='logoWrapper'>
+            <div className='logoImgWrapper'>
+              <Image width='150' height='150' src='/images/logo.png' alt='' />
+            </div>
+            <span className='logo1'>Lebon</span>
+            <span className='logo2'>job</span>
+          </div>
+        </Link>
+        <div className='btnsWrapper'>
+          <Link href='/employer/jobs/new'>
+            <button className='postAJobButton'>
+              <span>Post A Job</span>
+            </button>
+          </Link>
+
+          <Link href='/login'>
+            <button className='loginButtonHeader'>
+              <span>Login</span>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
